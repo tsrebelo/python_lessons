b7c17770f041cbd8bc861e80ade4899dc7406640
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-16 11:39:35 +0000

    updates

--- introducao/lacos_repeticao.py
+++ introducao/lacos_repeticao.py
@@ -1,2 +1,13 @@
+# Ciclo for
+
 for i in range(0, 10, 2):
     print("Repetição", i)
+    
+print("------------------")
+
+# Ciclo while
+
+i = 0
+while i < 10:
+    print("Repetição", i)
+    i += 1
