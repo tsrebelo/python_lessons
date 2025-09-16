c427aab59b201d0406301cebce802468a051b155
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-16 11:47:31 +0000

    updates

---
+++ introducao/funcoes.py
@@ -0,0 +1,20 @@
+def repetir():
+    i = 0
+    while i < 10:
+        print("Repetição", i)
+        i += 1
+    return
+    
+repetir()
+
+print("---------------")
+
+# Função com parametros
+def repetirVezes(vezes):
+    i = 0
+    while i < vezes:
+        print("Repetição", i)
+        i += 1
+    return
+
+repetirVezes(5)
