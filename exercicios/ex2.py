7283d02d0170afad75f60eb3ad962ec73c19d81b
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-19 12:04:53 +0000

    updates

--- exercicios/ex2.py
+++ exercicios/ex2.py
@@ -1,21 +1,21 @@
-""" 
-Exercício 2: Lista de Compras
 
-Cria uma lista chamada compras com 3 produtos à tua escolha
-(por exemplo "pão", "leite", "maçãs"). Pede ao utilizador para introduzir
-um novo produto e adiciona-o à lista.
-Guarda numa variável o número total de produtos da lista.
-Mostra na consola uma frase formatada como esta:
+# Exercício 2: Lista de Compras
 
-A tua lista de compras tem 4 produtos:
-pão, leite, maçãs, arroz.
+#Cria uma lista chamada compras com 3 produtos à tua escolha
+#(por exemplo "pão", "leite", "maçãs"). Pede ao utilizador para introduzir
+#um novo produto e adiciona-o à lista.
+#Guarda numa variável o número total de produtos da lista.
+#Mostra na consola uma frase formatada como esta:
 
-Dicas:
-Usa input() para ler o produto do utilizador.
-Para juntar os elementos da lista numa única frase podes usar
-", ".join(lista). Para formatação de strings podes usar f-strings:
+#A tua lista de compras tem 4 produtos:
+#pão, leite, maçãs, arroz.
 
-"""
+#Dicas:
+#Usa input() para ler o produto do utilizador.
+#Para juntar os elementos da lista numa única frase podes usar
+#", ".join(lista). Para formatação de strings podes usar f-strings:
+
+
 import os
 
 os.system('clear')
