829cb71e536e79203da1edfd9280628f7531e09d
telma <145616594+tsrebelo@users.noreply.github.com>
2025-09-24 13:08:25 +0000

    Updates

--- exercicios/ex8.py
+++ exercicios/ex8.py
@@ -1,26 +1,30 @@
 #
 # uma aplicação em python que consulte um api público e mostre os dados
 #
-# Sismos - https://api.ipma.pt/open-data/observation/seismic/{idArea}.json 
-# Localização - https://api.ipma.pt/open-data/sea-locations.json
+# Sismos - https://api.ipma.pt/open-data/observation/seismic/{idArea}.json
 #
-
 import requests
 
-def consultaIDLocadidade():
-
-    url = "https://api.ipma.pt/open-data/sea-locations.json"
+def main():
+    idLocalizacao = input("Escolha a região pretendida 3 - Arq. Açores | 7 - Continente, Arq. Madeira: ")
     
-    try:
-        resposta = requests.get(url)
-        
-        if resposta.status_code == 200:
-            return resposta.json()
-        else:
-            print(f"Erro ao obter localizações: {resposta.status_code}")
-            return None
-            
-    except requests.exceptions.RequestException as e:
-        print(f"Erro de conexão: {e}")
-        return None 
+    if idLocalizacao == "3" or idLocalizacao == "7":
+        url = f"https://api.ipma.pt/open-data/observation/seismic/{idArea}.json"
+        response = requests.get(url)
 
+        if response.status_code == 200:
+            sismos_data = response.json()
+            return sismos_data
+        else:
+            print(f"Erro ao obter dados: {response.status_code}")
+            return None
+    else:
+        print("Input incorreto! Tente novamente.")
+        return None
+
+data = main()
+if data:
+    print("Dados sísmicos obtidos: ", data)
+else:
+    print("Falha ao obter dados.")
+        
