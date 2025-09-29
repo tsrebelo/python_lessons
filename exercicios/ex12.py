13c943d6378e2e7be2a2ae9b770165a7e4dbf41d
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-29 15:15:42 +0000

    updates

---
+++ exercicios/ex12.py
@@ -0,0 +1,10 @@
+#
+# Programa com uma função que aceite como parâmetro
+# 2 sets e devolve a sua união
+#
+
+frutas = {"maçã", "pêra", "uva"}
+legumes = {"cenoura", "alface", "tomate"}
+
+setFinal = legumes.union(frutas)
+print(setFinal)
---
+++ exercicios/ex13.py
@@ -0,0 +1,16 @@
+#
+# Programa que aceita uma lista em matriz no formato:
+#   1   2   3 
+#   4   5   6
+#   7   8   9
+# e que apresente a lista no ecrã como o formato acima
+#
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for x in matriz:
+    print(x)
