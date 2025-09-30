ab8adfe01c023faa1f6ee9a9f2547f0f0927cfc9
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-30 10:33:37 +0000

    updates

--- exercicios/ex14_bingo.py
+++ exercicios/ex14_bingo.py
@@ -5,23 +5,37 @@
 import random
 import os
 
+# Reset
+RESET = "\033[0m"   
+
+# Regular Colors
+BLACK = "\033[0;30m"      
+RED = "\033[0;31m"      
+GREEN = "\033[0;32m"    
+YELLOW = "\033[0;33m"    
+BLUE = "\033[0;34m"     
+PURPLE = "\033[0;35m"      
+CYAN = "\033[0;36m"        
+WHITE = "\033[0;37m" 
+
 def mostrar_painel(numeros_sorteados):
-    print("\nPainel de Números Sorteados:")
+    os.system('clear') 
+    print(CYAN + "\nPainel de Números Sorteados:\n" + RESET)
     for i in range(1, 101):
         if i in numeros_sorteados:
-            print(f"{i:3}", end=" ")
+            print(PURPLE + f"{i:3}", end=" " + RESET)
         else:
-            print("---", end=" ")
+            print(PURPLE + "---", end=" " + RESET)
         if i % 10 == 0:
             print()
-        print()
+    print()
 
 def main():
     numeros = list(range(1, 101))
     random.shuffle(numeros)
     numeros_sorteados = set()
     
-    print("Bem-Vindo ao Jogo do Bingo!\n")
+    print(BLUE + "Bem-Vindo ao Jogo do Bingo!\n" + RESET)
     while len(numeros_sorteados) < 100:
         input("Prima ENTER para sortear o próximo número...")
         numero = numeros.pop()
@@ -29,7 +43,8 @@ def main():
         print(f"\nNúmero sorteado: {numero}\n")
         mostrar_painel(numeros_sorteados)
 
-    print("Todos os números foram sorteados! Fim do jogo.")
+    print(YELLOW + "Todos os números foram sorteados! Fim do jogo." + RESET)
     
-if __name__ == "__main__":      
+if __name__ == "__main__":    
+    os.system('clear')      
     main()
---
+++ exercicios/ex15.py
@@ -0,0 +1,24 @@
+#
+# Faz uma função que receba um set e dentro
+# da função, um a um, os elementos do set são 
+# transferidos para outro set
+#
+# setA = {1, 2, 3, 4, 5}
+# setB = {}
+# |
+# setA = {}
+# setB = {1, 2, 3, 4, 5}
+#
+
+setA = {1, 2, 3, 4, 5}
+setB = set()
+
+def transferirSets(x):
+    for x in range(len(setA)):
+        numero = setA.pop()
+        setB.add(numero)
+    print(setA)
+    print(setB)
+
+if __name__ == "__main__":
+    transferirSets(setA)
