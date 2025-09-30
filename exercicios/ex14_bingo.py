5b53190f7a39cf4d11c9f7dd0cfc21aa43a28d15
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-09-30 09:29:30 +0000

    updates

--- exercicios/ex14_bingo.py
+++ exercicios/ex14_bingo.py
@@ -3,6 +3,33 @@
 # 0-100 sortear sem repetir
 # 
 import random
+import os
 
-numSort = random.randint(0, 100)
-print(f"O número sorteado é: {numSort}")
+def mostrar_painel(numeros_sorteados):
+    print("\nPainel de Números Sorteados:")
+    for i in range(1, 101):
+        if i in numeros_sorteados:
+            print(f"{i:3}", end=" ")
+        else:
+            print("---", end=" ")
+        if i % 10 == 0:
+            print()
+        print()
+
+def main():
+    numeros = list(range(1, 101))
+    random.shuffle(numeros)
+    numeros_sorteados = set()
+    
+    print("Bem-Vindo ao Jogo do Bingo!\n")
+    while len(numeros_sorteados) < 100:
+        input("Prima ENTER para sortear o próximo número...")
+        numero = numeros.pop()
+        numeros_sorteados.add(numero)
+        print(f"\nNúmero sorteado: {numero}\n")
+        mostrar_painel(numeros_sorteados)
+
+    print("Todos os números foram sorteados! Fim do jogo.")
+    
+    if __name__ == "__main__":      
+        main()
