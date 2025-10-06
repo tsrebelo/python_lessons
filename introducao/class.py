993e4670e2bc4a82ee5af044d35bd10ff1a86ef4
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-10-06 15:07:37 +0000

    updates

--- introducao/class.py
+++ introducao/class.py
@@ -7,3 +7,20 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
+
+
+class Carro:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        print("O Construtor foi inicializado")
+        
+    def mostraMarca(self):
+        print(self.marca)
+        
+    def mostraModelo(self):
+        print(self.modelo)
+        
+c1 = Carro("BMW", "X3")
+c1.mostraMarca()
+c1.mostraModelo()
