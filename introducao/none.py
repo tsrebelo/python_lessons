458355b1c4dbf3e2304a0a61338f9fc4f8e414bf
Telma Silva Rebelo <rebelotelma01@gmail.com>
2025-10-06 14:41:22 +0000

    updates

---
+++ introducao/none.py
@@ -0,0 +1,11 @@
+def myfunc():
+    x = 5
+    
+x = myfunc()
+print(x)
+
+result = None
+if result is not None:
+    print("Result is ready")
+else:
+    print("No result yet")
---
+++ introducao/raise.py
@@ -0,0 +1,4 @@
+x = "hello"
+
+if not type(x) is int:
+    raise TypeError("Atenção porque a variável não é um inteiro")
